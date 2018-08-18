208826e6cbecb1a23ca219f70ee9f0bd2e29aad1
myNameiszhao <348526584@qq.com>
2018-08-18 09:40:02 +0800

    第二天

--- app/Http/Controllers/admin/MdataController.php
+++ app/Http/Controllers/admin/MdataController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers\admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\Mdata;
+use Input;
+use DB;
 
 class MdataController extends Controller
 {
-    //
     public function index(){
-        return view('admin.Mdata.index');
+        if(Input::method() == 'POST'){
+            $data1 = Input::all();
+            dump($data1);
+            die;
+        }else{
+            $data = [];
+            $user = DB::table("user")->get();
+            $game_name = DB::table('message')->get();
+            $data['user'] = $user;
+            $data['game_name'] = $game_name;        
+            return view('admin.Mdata.index',compact('data'));
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::group(['prefix' => 'admin','namespace' =>'admin','middleware'=>'login'], 
     Route::get('achievement/index','AchievementController@index')->name('achievement.index');
     Route::any('achievement/add','AchievementController@add')->name('achievement.add');
     //比赛数据管理
-    Route::get('Mdata/index','MdataController@index')->name('Mdata.index');
+    Route::any('Mdata/index','MdataController@index')->name('Mdata.index');
     Route::any('Mdata/edit','MdataController@edit')->name('Mdata.edit');
     Route::any('Excel/index','ExcelController@index')->name('excel.index');
     //管理员列表
